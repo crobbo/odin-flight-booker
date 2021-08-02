1b50353e13d9c4a5d75bd26a9a7b3e4c85a3c7e6
Christian <chrisdrobbo@gmail.com>
2021-08-02 21:08:39 +0100

    wrote method to query database and find flights that match the search criteria.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,16 +2,33 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
     @airports = Airport.all
+    if params[:to_code] 
+      find_flights 
+    else
+      return nil
+    end
   end
-  
+
   private 
 
-  def set_flight
-    @flight = Flight.find(params[:id])
+  def find_flights
+    airport_from_id = Airport.where(code: params[:from_code])[0][:id]
+    airport_to_id = Airport.where(code: params[:to_code])[0][:id]
+    @flights = Flight.where(from_airport_id: airport_from_id, to_airport_id: airport_to_id)
+    @same_day_flights = []
+    @flights.each do |flight|
+      flight[:scheduled_for].strftime("%Y/%m/%d")
+      @same_day_flights << flight
+    end
   end
+
+  # def flight_date_formatted
+  #   params[:].strftime("%m/%d/%Y")
+  # end
+
+  private
 
   def flight_params
     params.requrie(:flight).permit(:scheduled_for, :duration, :to_airport_id, :from_airport_id)
   end
 end
-
