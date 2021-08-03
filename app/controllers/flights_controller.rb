8a11c3f4b32256ed0527be52c5af02132fc10755
Christian <chrisdrobbo@gmail.com>
2021-08-03 20:27:47 +0100

    Added feature which displays flight search results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,32 +1,31 @@
 class FlightsController < ApplicationController
+
+
+
   def index
     @flights = Flight.all
     @airports = Airport.all
     if params[:to_code] 
-      find_flights 
+      find_flights
     else
       return nil
     end
   end
 
-  private 
+  private
 
   def find_flights
     airport_from_id = Airport.where(code: params[:from_code])[0][:id]
     airport_to_id = Airport.where(code: params[:to_code])[0][:id]
-    @flights = Flight.where(from_airport_id: airport_from_id, to_airport_id: airport_to_id)
+    @flights_match = Flight.where(from_airport_id: airport_from_id, to_airport_id: airport_to_id)
+
     @same_day_flights = []
-    @flights.each do |flight|
+
+    @flights_match.each do |flight|
       flight[:scheduled_for].strftime("%Y/%m/%d")
       @same_day_flights << flight
     end
   end
-
-  # def flight_date_formatted
-  #   params[:].strftime("%m/%d/%Y")
-  # end
-
-  private
 
   def flight_params
     params.requrie(:flight).permit(:scheduled_for, :duration, :to_airport_id, :from_airport_id)
