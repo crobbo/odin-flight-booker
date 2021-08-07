f8c03df2f2003e15f88503d42b85b25c9408be4b
Christian <chrisdrobbo@gmail.com>
2021-08-07 20:45:20 +0100

    added flight list page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -17,17 +17,18 @@ class FlightsController < ApplicationController
   def find_flights
     airport_from_id = Airport.where(code: params[:from_code])[0][:id]
     airport_to_id = Airport.where(code: params[:to_code])[0][:id]
-    @flights_match = Flight.where(from_airport_id: airport_from_id, to_airport_id: airport_to_id)
 
+    @flights_match = Flight.where(from_airport_id: airport_from_id, to_airport_id: airport_to_id)
     @same_day_flights = []
 
     @flights_match.each do |flight|
       flight[:scheduled_for].strftime("%Y/%m/%d")
       @same_day_flights << flight
     end
+
   end
 
-  def flight_params
-    params.requrie(:flight).permit(:scheduled_for, :duration, :to_airport_id, :from_airport_id)
-  end
+  # def flight_params
+  #   params.require(:flight).permit(:scheduled_for, :duration, :to_airport_id, :from_airport_id)
+  # end
 end
